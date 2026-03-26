ff438e2bef30cc1a884539a43397160984e70193
Rapikku <alvarolapike12@gmail.com>
2026-03-26 19:10:07 +0100

    Actualizada la cabecera principal de la aplicación

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        GestorTareas gestor = new GestorTareas();
-        System.out.println("=== GESTOR DE TAREAS ===");
+    public static void main(String[] args) { GestorTareas gestor = new GestorTareas();
+        System.out.println("=== ORGANIZADOR PERSONAL DE TAREAS ===");
         gestor.listarTareas();
-        //comentario para mostrar el commit
     }
-}
+} 
